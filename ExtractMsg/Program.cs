7e7b8a116c57e9fe0eac1740ad60e3520787cc77
Ruben Steins <rubensteins@gmail.com>
2025-02-18 15:22:17 +0100

    Added support for Encoding1252, added version param.

--- ExtractMsg/Program.cs
+++ ExtractMsg/Program.cs
@@ -1,4 +1,7 @@
 ﻿using MsgReader.Outlook;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 bool showDiag = false; // show diagnostic messages
 bool useFolder = false; // no seperate folder
@@ -16,62 +19,69 @@ else
 
 if (!File.Exists(fileName))
 {
-    PrintHelp("Invalid file name.");
+    PrintHelp("File not found.");
     Environment.Exit(-1);
 }
 
 List<object> messagesToProcess = new List<object>();
 string basePath = "";
 
-// Open the Message
-using (var msg = new Storage.Message(fileName))
+try
 {
-    messagesToProcess.Add(msg);
-    if (useFolder)
+    // Open the Message
+    using (var msg = new Storage.Message(fileName))
     {
-        basePath = msg.Subject + Path.DirectorySeparatorChar;
-        var dir = Directory.CreateDirectory(basePath);
-        if (showDiag)
+        messagesToProcess.Add(msg);
+        if (useFolder)
         {
-            Console.WriteLine($"Created folder {dir.Name}");
+            basePath = msg.Subject + Path.DirectorySeparatorChar;
+            var dir = Directory.CreateDirectory(basePath);
+            if (showDiag)
+            {
+                Console.WriteLine($"Created folder {dir.Name}");
+            }
         }
-    }
-    
-    while (messagesToProcess.Count > 0)
-    {
-        // if it's a message, write its text.
-        if (messagesToProcess[0] is Storage.Message)
+        
+        while (messagesToProcess.Count > 0)
         {
-            Storage.Message message = (messagesToProcess[0] as Storage.Message)!;
+            // if it's a message, write its text.
+            if (messagesToProcess[0] is Storage.Message)
+            {
+                Storage.Message message = (messagesToProcess[0] as Storage.Message)!;
 
-            File.WriteAllText($"{basePath}{message.Subject}.txt", message.BodyText);
-            messagesToProcess.RemoveAt(0);
-            
-            // msg might contain attachments, process them as well
-            if (message.Attachments.Count > 0)
-            {
-                messagesToProcess.AddRange(message.Attachments);
-            }
-        }
-        else
-        {
-            // process attachment
-            Storage.Attachment a = (messagesToProcess[0] as Storage.Attachment)!;
-            if (a != null)
-            {
-                var fileToWrite = $"{basePath}{a.FileName}";
-                if (File.Exists($"{basePath}{a.FileName}"))
-                {
-                    // Sometimes nested files have the same name :(
-                    fileToWrite = $"{basePath}copy of {a.FileName}";
-                }
+                File.WriteAllText($"{basePath}{message.Subject}.txt", message.BodyText);
+                messagesToProcess.RemoveAt(0);
                 
-                File.WriteAllBytes(fileToWrite, a.Data);
-                if(showDiag) Console.WriteLine($"Wrote {a.Data.Length} bytes to {a.FileName}");
+                // msg might contain attachments, process them as well
+                if (message.Attachments.Count > 0)
+                {
+                    messagesToProcess.AddRange(message.Attachments);
+                }
             }
-            messagesToProcess.RemoveAt(0);
+            else
+            {
+                // process attachment
+                Storage.Attachment a = (messagesToProcess[0] as Storage.Attachment)!;
+                if (a != null)
+                {
+                    var fileToWrite = $"{basePath}{a.FileName}";
+                    if (File.Exists($"{basePath}{a.FileName}"))
+                    {
+                        // Sometimes nested files have the same name :(
+                        fileToWrite = $"{basePath}copy of {a.FileName}";
+                    }
+                    
+                    File.WriteAllBytes(fileToWrite, a.Data);
+                    if(showDiag) Console.WriteLine($"Wrote {a.Data.Length} bytes to {a.FileName}");
+                }
+                messagesToProcess.RemoveAt(0);
+            }
         }
     }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"There was an unhandled error of type {e.GetType()}: {e.Message}");
 }
 
 if(showDiag) Console.WriteLine("Done.");
@@ -101,6 +111,10 @@ void ProcessArguments()
                     Environment.Exit(0);
                     break;
                 }
+                case "-v":
+                    PrintVersionInfo();
+                    Environment.Exit(0);
+                    break;
                 default:
                 {
                     PrintHelp($"Unknown parameter {args[c]}");
@@ -125,13 +139,19 @@ void PrintHelp(string error = "")
     }
     Console.WriteLine("ExtractMsg will open up a .msg message.");
     Console.WriteLine("Body text will be written as text-file and (nested) attachments exported.");
-    Console.WriteLine("(c) 2024 Ruben Steins - MIT License");
+    Console.WriteLine("(c) 2024,2025 Ruben Steins - MIT License");
     Console.WriteLine("");
     Console.WriteLine("Usage:");
-    Console.WriteLine("ExtractMsg filename [-f] [-d] [-h]");
+    Console.WriteLine("ExtractMsg <filename> [-d] [-f] [-h] [-v]");
     Console.WriteLine();
-    Console.WriteLine("filename:          The name of the .msg file to extract");
-    Console.WriteLine("-f     Folder.     Create a new folder called 'filename' and extract into that.");
+    Console.WriteLine("filename:          The name of the .msg file to extract.");
     Console.WriteLine("-d     Diagnostic. Show some diagnostic messages.");
+    Console.WriteLine("-f     Folder.     Create a new folder called 'filename' and extract into that.");
     Console.WriteLine("-h     Help.       Shows this text.");
+    Console.WriteLine("-v     Version.    Print version info for this application.");
+}
+
+void PrintVersionInfo()
+{
+    Console.WriteLine("ExtractMsg - Version 1.1 - 20250218");
 }
